6e6ff8696a67ad609825af6c4a8c687f7a431973
Carlos Astorga <castorga@car14s6.42barcelona.com>
2023-12-28 16:03:30 +0100

    first commit

---
+++ philo/src/main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: castorga <castorga@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 15:54:06 by castorga          #+#    #+#             */
+/*   Updated: 2023/12/28 15:54:09 by castorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+memset, printf, malloc, free, write,
+usleep, gettimeofday, pthread_create,
+pthread_detach, pthread_join, pthread_mutex_init,
+pthread_mutex_destroy, pthread_mutex_lock,
+pthread_mutex_unlock	
+*/
