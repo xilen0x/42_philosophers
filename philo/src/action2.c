3426ee4e2492b6052dbe8b0f0a187a92ba160c24
Carlos Astorga <castorga@cbr1s4.42barcelona.com>
2024-02-08 20:05:49 +0100

    fixed monitor

---
+++ philo/src/action2.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   action2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: castorga <castorga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 17:38:58 by castorga          #+#    #+#             */
+/*   Updated: 2024/02/08 19:41:55 by castorga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*function GETTER that check the value of its_alive variable*/
+int	get_its_alive(t_chrono *ch)
+{
+	int	alive;
+
+	pthread_mutex_lock(&ch->mutex_its_alive);
+	alive = ch->its_alive;
+	pthread_mutex_unlock(&ch->mutex_its_alive);
+	return (alive);
+}
+
+/*function SETTER that change the its_alive variable*/
+void	set_its_alive(t_chrono *ch)
+{
+	pthread_mutex_lock(&ch->mutex_its_alive);
+	ch->its_alive = 0;
+	pthread_mutex_unlock(&ch->mutex_its_alive);
+}
